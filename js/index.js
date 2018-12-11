b3380eb2f530b05c8e8a82bdb64e94c7f3703161
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2018-12-11 11:35:15 +0000

    set up the reset button

--- js/index.js
+++ js/index.js
@@ -21,6 +21,49 @@ function setupModeButtons() {
     }
 }
 
+function setupSquares() {
+    for (var i = 0; i < squares.length; i++) {
+        //add click listeners to squares
+        squares[i].addEventListener("click", function() {
+            //grab color of clicked square
+            var clickedColor = this.style.background;
+            //compare color to pickedColor
+            if (clickedColor === pickedColor) {
+                messageDisplay.textContent = "Correct!";
+                resetButton.textContent = "Play Again?"
+                changeColors(clickedColor);
+                h1.style.background = clickedColor;
+            } else {
+                this.style.background = "#232323";
+                messageDisplay.textContent = "Try Again"
+            }
+        });
+    }
+}
+
+function reset() {
+    colors = generateRandomColors(numSquares);
+    //pick a new random color from array
+    pickedColor = pickColor();
+    //change colorDisplay to match picked Color
+    colorDisplay.textContent = pickedColor;
+    resetButton.textContent = "New Colors"
+    messageDisplay.textContent = "";
+    //change colors of squares
+    for (var i = 0; i < squares.length; i++) {
+        if (colors[i]) {
+            squares[i].style.display = "block"
+            squares[i].style.background = colors[i];
+        } else {
+            squares[i].style.display = "none";
+        }
+    }
+    h1.style.background = "steelblue";
+}
+
+resetButton.addEventListener("click", function() {
+    reset();
+})
 
 function changeColors(color) {
     for (let i = 0; i < squares.length; i++) {
