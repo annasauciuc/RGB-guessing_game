6781fde62a509f762e8ecda7961562b2ae6a36a2
annasauciuc <sauciuc.anamaria.dd@gmail.com>
2019-02-03 16:15:33 +0000

    changimg color of spans and text weigth

--- js/index.js
+++ js/index.js
@@ -44,7 +44,7 @@ function setupSquares() {
                 changeColors(clickedColor);
                 h1.style.background = clickedColor;
             } else {
-                this.style.background = "#FFF";
+                this.style.background = "#232323";
                 messageDisplay.textContent = "Try Again"
             }
         });
